de67a442300df78efe7cc7e030e9e3f3a7b90d9c
heytoshi <enkhbat_tsogt@yahoo.com>
2018-04-15 00:22:24 -0600

    Release 2.5

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Mono.Data.SqliteClient;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour {
+
+    public Text scoreText;
+	public Text tokenText;
+
+    public Image backgroundImage;
+    private bool isShowed = false;
+    private float transition = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isShowed)  {
+            return;
+        }
+        transition += Time.deltaTime;
+        backgroundImage.color = Color.Lerp(new Color(0,0,0,0),Color.black,transition);
+	}
+
+    public void ToggleEndMenu(float score) {
+        gameObject.SetActive(true);
+        scoreText.text = ((int)score).ToString();
+        isShowed = true;
+    }
+
+    public void Restart() {
+		SceneManager.LoadScene("Game");
+		//direct db connection to where the db is stored in app
+		//and open connection
+		const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
+		IDbConnection dbcon = new SqliteConnection(connectionString);
+		dbcon.Open();
+		IDbCommand dbcmd = dbcon.CreateCommand();
+
+		//create query for adding score
+		//dbcmd = null;
+		dbcon.CreateCommand();
+		const string command =
+		"INSERT INTO Score " +
+		"(userID, totalScore, balance, grade) " +
+		"VALUES (@two, @three, @four, @five)";
+
+		//dbcmd.Parameters.Add(new SqliteParameter("@one", one)); 
+		dbcmd.Parameters.Add(new SqliteParameter("@two", SubmitName.getStuID()));
+		dbcmd.Parameters.Add(new SqliteParameter("@three", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@four", Int32.Parse(tokenText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@five", SubmitName.getStuGrade()));
+
+		string sql = command;
+		Debug.Log(sql);
+		Debug.Log(SubmitName.getTeachID());
+		Debug.Log(scoreText.text);
+		Debug.Log (tokenText.text);
+		Debug.Log(SubmitName.getStuGrade());
+
+		dbcmd.CommandText = command;
+		IDataReader reader = dbcmd.ExecuteReader();
+
+	}
+
+	public void ToMenu() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Menu");
+		//direct db connection to where the db is stored in app
+		//and open connection
+		const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
+		IDbConnection dbcon = new SqliteConnection(connectionString);
+		dbcon.Open();
+		IDbCommand dbcmd = dbcon.CreateCommand();
+
+		//create query for adding score
+		dbcmd = null;
+		dbcon.CreateCommand();
+		const string command =
+			"INSERT INTO Score " +
+			"(userID, totalScore, balance, grade) " +
+			"VALUES (@two, @three, @four, @five)";
+
+		//dbcmd.Parameters.Add(new SqliteParameter("@one", one)); 
+		dbcmd.Parameters.Add(new SqliteParameter("@two", SubmitName.getStuID()));
+		dbcmd.Parameters.Add(new SqliteParameter("@three", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@four", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@five", SubmitName.getStuGrade()));
+
+		string sql = command;
+		Debug.Log(sql);
+		Debug.Log(SubmitName.getTeachID());
+		Debug.Log(scoreText.text);
+		Debug.Log (tokenText.text);
+		Debug.Log(SubmitName.getStuGrade());
+		dbcmd.CommandText = command;
+		IDataReader reader = dbcmd.ExecuteReader();
+
+	} 
+		
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,114 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public static float score = 0.0f;
+    private int difficultyLevel = 1;
+    private int maxDifficultyLevel = 2;
+    private int scoreToNextLevel = 100;
+    private bool isDeath = false;
+    public Text scoreText;
+    public DeathMenu deathMenu;
+
+
+
+    public object FindData(string tableName, string selectHeader, string whereHeader, object whereValue)
+    {
+        using (var dbConnection = new SqliteConnection("URI=file:" + Application.dataPath + "/Plugins/MumboJumbos.db"))
+        {
+            dbConnection.Open();
+
+            using (var dbCommand = dbConnection.CreateCommand())
+            {
+                dbCommand.CommandText = string.Format("SELECT {0} " +
+                                                       "FROM {1} " +
+                                                       "WHERE {2} = @whereValue", selectHeader, tableName, whereHeader);
+
+                SqliteParameter whereParam = new SqliteParameter("@whereValue", whereValue);
+                dbCommand.Parameters.Add(whereParam);
+
+                using (var dbReader = dbCommand.ExecuteReader())
+                {
+                    while (dbReader.Read())
+                    {
+                        if (dbReader.GetValue(0) != null)
+                            return dbReader.GetValue(0);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void UpdateData(string tableName, string whereHeader, object whereValue, string setHeader, object setValue)
+    {
+        using (var dbConnection = new SqliteConnection("URI=file:" + Application.dataPath + "/Plugins/MumboJumbos.db"))
+        {
+            dbConnection.Open();
+
+            using (var dbCommand = dbConnection.CreateCommand())
+            {
+
+                dbCommand.CommandText = string.Format("UPDATE {0} " + "SET {1} = @setValue " + "WHERE {2} = @whereValue",
+                    tableName, setHeader, whereHeader);
+
+                SqliteParameter setParam = new SqliteParameter("@setValue", setValue);
+                SqliteParameter whereParam = new SqliteParameter("@whereValue", whereValue);
+
+                dbCommand.Parameters.Add(setParam);
+                dbCommand.Parameters.Add(whereParam);
+
+                Debug.Log(dbCommand.CommandText);
+
+                dbCommand.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(isDeath){
+            return;
+        }
+        if(score >= scoreToNextLevel) {
+           // LevelUp();
+        }
+        score += Time.deltaTime;
+        scoreText.text = ((int)score).ToString();
+    }
+
+    void LevelUp() {
+        if(difficultyLevel == maxDifficultyLevel) {
+            return;
+        }
+        scoreToNextLevel *= 2;
+        difficultyLevel++;
+        GetComponent<CharacterControl>().SetSpeed(difficultyLevel);
+
+        Debug.Log(difficultyLevel);
+    }
+
+    public void OnDeath() {
+        isDeath = true;
+        PlayerPrefs.SetFloat("Highscore", score);
+        deathMenu.ToggleEndMenu (score);
+        var hscore = FindData("student", "Score", "StuID", SubmitName.getStuID());
+        int hscores = Convert.ToInt32(hscore);
+        if (hscores < score) {
+            UpdateData("student", "StuID", SubmitName.getStuID(), "Score", score);
+        }
+        Score.score = 0;
+    }
+}
